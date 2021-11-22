b96d654747dfa9257d0e0f41481de2e32f121999
Rahul2040 <rajaramrahul2@gmail.com>
2021-11-22 13:12:58 +0530

    rahul's 3rd commit

--- src/app/config/constant.ts
+++ src/app/config/constant.ts
@@ -15,6 +15,8 @@ export class Constant {
     public Question_API='http://localhost:8080/api/question';
 
     public AssessmentArea_API='http://localhost:8080/api/AssessmentArea';
+    public Question_Option_Api="http://localhost:8080/api/questionoption"
+    public Question_Type_Api="http://localhost:8080/api/question_type"
 
     // public Category_API="http://localhost:8080/api/Category"
 
